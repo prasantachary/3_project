aeca034f348f3a59bd232f2935d5a88c427f17bd
prasantachary <kumarprasant090@gmail.com>
2023-11-10 00:40:55 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def kanha(request):
+    return render(request,'index.html')
+def kanha1(request):
+    return render(request,'index2.html')
+def kanha2(request):
+    return render(request,'index3.html')
+def kanha3(request):
+    return render(request,'index4.html')
